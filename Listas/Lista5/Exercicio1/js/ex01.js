6967b270111d1d325c5204982dfe70de682362eb
natanSMS <natansms07@gmail.com>
2024-05-24 22:51:27 -0300

    Lista 5 - Primeiros passos com JS

---
+++ Listas/Lista5/Exercicio1/js/ex01.js
@@ -0,0 +1,6 @@
+var nome = prompt("Digite seu nome:")
+    var idade = prompt("Digite sua idade:")
+    var empregoAtual = prompt("Digite seu emprego atual:")
+    var endereco = prompt("Digite seu endereço:")
+
+    alert(`seu nome é ${nome} \nsua idade é de ${idade} anos \nseu emprego é ${empregoAtual} \nseu endereço é ${endereco}`)
---
+++ Listas/Lista5/Exercicio2/js/ex02.js
@@ -0,0 +1,6 @@
+var nome = "Paulo"
+var idade = 23
+var empregoAtual = "Professor de inglês"
+var endereco = "rua Rabisco 324"
+
+console.log(`Seu nome é ${nome} \nVocê tem ${idade} anos \nSeu emprego atual é: ${empregoAtual} \nE mora no endereço: ${endereco}`)
